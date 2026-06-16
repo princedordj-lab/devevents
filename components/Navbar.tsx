1c7eaeffe26f0501dd0a5cc6f1e2a35994351ef0
princedordj-lab <princedordj@gmail.com>
2026-06-16 00:52:55 +0000

    Implement responsive toggle menu in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { MenuIcon } from "lucide-react";
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="sticky top-0 z-10">
       <nav className=" flex items-center justify-between bg-black/40 py-4 max-sm:py-5 px-4 backdrop-blur-md">
@@ -16,11 +21,23 @@ const Navbar = () => {
           <p className="text-3xl max-sm:text-xl font-bold">DevEvent</p>
         </Link>
 
-        <div className="flex space-x-4 max-md:space-x-2">
+        <div className="flex space-x-4 max-md:space-x-2 max-sm:hidden">
           <Link href={"/"}>Home</Link>
           <Link href={"/events"}>Events</Link>
           <Link href={"/create"}>Create Event</Link>
         </div>
+
+        <button onClick={() => setToggle(!toggle)} className="lg:hidden sm:hidden">
+          <MenuIcon className="text-white" />
+        </button>
+
+        {toggle && (
+          <div className="absolute left-0 top-19 w-full bg-black/70 backdrop-blur-2xl rounded-b-lg p-4 flex flex-col space-y-2 max-sm:space-y-7 items-center justify-center lg:hidden sm:hidden">
+            <Link href={"/"} onClick={() => setToggle(false)}>Home</Link>
+            <Link href={"/events"} onClick={() => setToggle(false)}>Events</Link>
+            <Link href={"/create"} onClick={() => setToggle(false)}>Create Event</Link>
+          </div>
+        )}
       </nav>
     </div>
   );
